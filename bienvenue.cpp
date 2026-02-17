3d2b82b5ad74b5e5301f18a1783057aaf86be2b2
nourouma <nourouma@gmail.com>
2026-02-17 09:26:29 +0100

    Ajout du fichier README.md

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,6 +1,8 @@
-// TODO Indiquer ce que fait le programme
+#include <iostream>
+
 int main()
 {
-// TODO Afficher un message de bienvenue
+std::cout << "Bienvenue le monde !" << std::endl;
 return 0;
 }
+
